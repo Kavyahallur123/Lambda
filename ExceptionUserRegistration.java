94c5dd6922c395a38513e7062177cc770cfe1c9f
kavyahallur123 <kavyahallur199@gmail.com>
2022-11-06 14:49:23 +0530

    Added; At least one special character

--- ExceptionUserRegistration.java
+++ ExceptionUserRegistration.java
@@ -5,15 +5,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * As a User need to follow predefined Password rules.
+ * As a User need to follow pre-defined Password rules.
  * UC5- Rule1– minimum 8 Characters
  * UC6- Rule2 – Should have at least 1 Upper Case
  * UC7- Rule3- Should have at least 1 numeric number in the password
- *
+ * UC8- Rule4- Should have at Exactly 1 Special Character in the password
  * @author user-Veeresh
  *
  */
 public class ExceptionUserRegistration {
+
 
     /**
      * created method PasswordRule1 and passing parameter in this it will check
@@ -22,7 +23,6 @@ public class ExceptionUserRegistration {
      *
      * @return -return to method created
      */
-
     public boolean passwordRule1(String str) {
         Pattern pattern = Pattern.compile("[a-zA-Z]{8,}");
         Matcher matcher = pattern.matcher(str);
@@ -34,10 +34,8 @@ public class ExceptionUserRegistration {
      * Password is valid or not result will be in true or false because method is
      * boolean type
      *
-     * @param str- passing password
      * @return -return to method created
      */
-
     public boolean passwordRule2(String str) {
         Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z]).{8,}$");
         Matcher matcher = pattern.matcher(str);
@@ -50,10 +48,26 @@ public class ExceptionUserRegistration {
      * be capital with one numeric number in that result will be in true or false
      * because method is boolean type
      *
+     * @param str- passing password
      * @return -return to method created
      */
     public boolean passwordRule3(String str) {
         Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.matches();
+    }
+
+    /**
+     * created method PasswordRul4 and passing parameter in this it will check
+     * Password is valid or not having Minimum 8 Characters and first letters should
+     * be capital with one numeric number and have one Exactly Special Character in
+     * that result will be in true or false because method is boolean type
+     *
+     * @param str- passing password
+     * @return -return to method created
+     */
+    public boolean passwordRule4(String str) {
+        Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[*.!@#$%^&(){}:'<>,.>/~`_+=|].).{8,}$");
         Matcher matcher = pattern.matcher(str);
         return matcher.matches();
     }
